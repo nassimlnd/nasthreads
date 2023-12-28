c21f6988885362d08e80898780ae002a48861e71
Nassim LOUNADI <nassimlnd37@gmail.com>
2023-12-28 14:43:35 +0100

    feat(authentication): added api routes for creation and authentication of an user

---
+++ app/api/user/auth/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/lib/db";
+import { hashPassword } from "@/app/api/user/create/route";
+import {NextResponse} from "next/server";
+
+export async function POST(req: Request, res: NextResponse) {
+    const {email, password} = await req.json();
+
+    if (!email || !password) {
+        return NextResponse.json({message: "Please provide an email and password"}, {status: 400});
+    }
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+                password: true,
+                image: true
+            }
+        });
+
+        if (user && user.password === hashPassword(password)) {
+            return NextResponse.json(exclude(user, ["password"]), {status: 200});
+        } else {
+            return NextResponse.json({message: "Invalid credentials"}, {status: 401});
+        }
+    } catch (e : any) {
+        throw new Error(e);
+    }
+}
+
+function exclude(user : any, keys : any) {
+    for (let key of keys) {
+        delete user[key];
+    }
+    return user;
+}
---
+++ app/api/user/create/route.ts
@@ -0,0 +1,48 @@
+import { SHA256 as sha256 } from "crypto-js";
+import prisma from "@/lib/db";
+import {Prisma} from "@prisma/client";
+import {NextApiRequest, NextApiResponse} from "next";
+import {NextRequest, NextResponse} from "next/server";
+
+export const hashPassword = (string: string) => {
+    return sha256(string).toString();
+}
+
+export async function POST(req: Request, res: NextResponse) {
+    let errors = [];
+    const {
+        name,
+        username,
+        email,
+        password
+    } = await req.json();
+
+    if (password.length < 8) {
+        errors.push("Password must be at least 8 characters long");
+        return NextResponse.json({message: errors}, {status: 400});
+    }
+
+    try {
+        const user = await prisma.user.create({
+            data: {
+                name: name,
+                username: username,
+                email: email,
+                password: hashPassword(password)
+            }
+        }).catch(console.error);
+
+        if (user) {
+            console.log(`USER CREATED : id: ${user.id}`);
+        }
+
+        return NextResponse.json({user}, {status: 201});
+    } catch (e) {
+        if (e instanceof Prisma.PrismaClientKnownRequestError) {
+            if (e.code === "P2002") {
+                return NextResponse.json({message: e.message}, {status: 400});
+            }
+            return NextResponse.json({message: e.message}, {status: 400});
+        }
+    }
+}
