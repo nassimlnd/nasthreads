0e8ba4b3c57d22a1fa1ba5cd945d985321a39019
Nassim LOUNADI <nassimlnd37@gmail.com>
2023-12-30 02:41:32 +0100

    feat(authentication): add temporary access control to redirect automatically to the auth page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,12 @@
 import Sidebar from "@/components/main/sidebar/sidebar";
+import {redirect} from "next/navigation";
 
 export default async function HomePage() {
+    let user;
+    if (!user) {
+        redirect("/auth");
+    }
+
     return (
         <main
             className="flex h-full w-3/4 mx-auto">
